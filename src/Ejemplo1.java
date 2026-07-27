018b3a9e0c927167c2499ccbfe28622c30143972
JCastagu <juanes121208@gmail.com>
2026-07-27 07:09:21 -0500

    Realice el ejemplo 1: int numero = 1;

---
+++ src/Ejemplo1.java
@@ -0,0 +1,10 @@
+public class Ejemplo1 {
+    public static void main(String[] args){
+        int numero = 1;
+
+        do {
+            System.out.println("El número es: " + numero);
+            numero++;
+        } while (numero <= 3);
+    }
+}
